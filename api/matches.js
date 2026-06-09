2b095dc2171bdcb07b6d99281f1972fe660091dc
Gerrard Wantah <gerrardwantah16@gmail.com>
2026-06-10 02:05:13 +1000

    fix CORS 2

--- api/matches.js
+++ api/matches.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end()
+    }
+  
     const response = await fetch('https://api.football-data.org/v4/competitions/WC/matches', {
       headers: {
         'X-Auth-Token': process.env.VITE_FOOTBALL_API_KEY
